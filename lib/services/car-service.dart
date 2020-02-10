63b51db6cffcb4f8064892258575bc688e0b5d87
welrikyson felix <welrikyson_felix@outlook.com>
2020-02-09 23:22:46 -0400

    seacher for vehicle

---
+++ lib/services/car-service.dart
@@ -0,0 +1,23 @@
+import 'package:car_wash/models/vehicle-model.dart';
+
+class VehicleService {
+  static Future<List<Vehicle>> getBy(String query) async => await Mock.getByQuery(query);
+}
+
+class Mock {
+  static Future<List<Vehicle>> getByQuery(String query) {    
+    Future<List<Vehicle>> futureListResults = Future(() {
+      return vehicles.where((element) => element.name.contains(query)).toList();
+    });        
+    return futureListResults;
+  }
+}
+
+final List<Vehicle> vehicles = [
+  Vehicle(name: "Uno", plate: "NOF-0566"),
+  Vehicle(name: "Ferrari", plate: "NOP-0566"),
+  Vehicle(name: "Lamborgni", plate: "XOV-0566"),
+  Vehicle(name: "Limosine", plate: "XZO-0566"),
+  Vehicle(name: "Fusca", plate: "EOP-0566"),
+  Vehicle(name: "Toro", plate: "XYZ-0566"),
+];
--- lib/widgets/dialog_search.dart
+++ lib/widgets/dialog_search.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:car_wash/models/vehicle-model.dart';
+import 'package:car_wash/services/car-service.dart';
 import 'package:flutter/material.dart';
 
 class DialogSearch extends StatefulWidget {
@@ -6,60 +10,112 @@ class DialogSearch extends StatefulWidget {
 }
 
 class _DialogSearchState extends State<DialogSearch> {
+  final TextEditingController _searchQuery = TextEditingController();
+  bool _isSearching = false;
+  String _error;
+  List<Vehicle> _results = List();
+
+  Timer debounceTimer;
+
+  _DialogSearchState() {
+    _searchQuery.addListener(() {
+      if (debounceTimer != null) {
+        debounceTimer.cancel();
+      }
+      debounceTimer = Timer(Duration(milliseconds: 500), () {
+        if (this.mounted) {
+          performSearch(_searchQuery.text);
+        }
+      });
+    });
+  }
+
+  void performSearch(String query) async {
+    if (query.isEmpty) {
+      setState(() {
+        _isSearching = false;
+        _error = null;
+        _results = List();
+      });
+      return;
+    }
+
+    setState(() {
+      _isSearching = true;
+      _error = null;
+      _results = List();
+    });
+
+    final repos = await VehicleService.getBy(query);
+    if (this._searchQuery.text == query && this.mounted) {
+      setState(() {
+        _isSearching = false;
+        if (repos != null) {
+          _results = repos;
+        } else {
+          _error = 'Error searching repos';
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      child: Container(          
-          margin: EdgeInsets.all(8),
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Text(
-                  "Veículos",
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                TextField(
-                  autofocus: true,
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Placa ou nome",
-                      isDense: true),
-                ),
-                SizedBox(
-                  height: 300,
-                  child: ListView(
-                    children: <Widget>[
-                      ListTile(
-                        subtitle: Text("Geral"),
-                        title: Text("Geral"),
-                        onTap: () {
-                          Navigator.pop(context, "Geral");
-                        },
-                      ),
-                      ListTile(
-                        subtitle: Text("GOl VW"),
-                        title: Text("XYX-1111"),
-                        onTap: () {
-                          Navigator.pop(context, "XYX-1111");
-                        },
-                      ),
-                      ListTile(
-                        subtitle: Text("Fiat Uno"),
-                        title: Text("NOP-0000"),
-                        onTap: () {},
-                      ),
-                    ],
-                  ),
-                ),
-                FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.add),
-                    label: Text("Adicionar")),
-              ],
-            ),
+      child: Container(
+        margin: EdgeInsets.all(8),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Text(
+                "Veículos",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              TextField(
+                controller: _searchQuery,
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Placa ou nome",
+                    isDense: true),
+              ),
+              SizedBox(
+                height: 300,
+                child: buildBody(context),
+              ),
+              FlatButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                  label: Text("Adicionar")),
+            ],
           ),
         ),
+      ),
     );
+  }
+
+  Widget buildBody(BuildContext context) {
+    if (_isSearching) {
+      return Center(child: Text('Buscando na api...'));
+    } else if (_error != null) {
+      return Text(_error);
+    } else if (_searchQuery.text.isEmpty) {
+      return Text('Inicie a busca digitando na barra de busca');
+    } else {
+      return ListView.builder(
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          itemCount: _results.length,
+          itemBuilder: (BuildContext context, int index) {
+            var result = _results[index];
+            return ListTile(
+              subtitle: Text(result.name),
+              title: Text(result.plate),
+              onTap: () {
+                Navigator.pop(context, result.plate);
+              },
+            );
+          });
+    }
   }
 }

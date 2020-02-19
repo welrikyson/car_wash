b67d5e2231409d206e8ea01e0de6985d9b36d4fa
welrikyson felix <welrikyson_felix@outlook.com>
2020-02-19 07:14:57 -0400

    modules segragation

---
+++ lib/app/modules/order/new_order/new_order_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddOrderWidget extends StatefulWidget {
+  @override
+  _AddOrderWidgetState createState() => _AddOrderWidgetState();
+}
+
+class _AddOrderWidgetState extends State<AddOrderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
